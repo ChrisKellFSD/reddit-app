db25d473f2c4a4083433ee389e2b7dd2652fe34b
ChrisKellFSD <chriskellcsn@gmail.com>
2023-04-15 21:49:14 +0000

    Add IBM flex sans font to app.module.css and apply to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Container from './components/Container';
 import NavBar from './components/NavBar';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavBar />
       <Container />
     </div>
